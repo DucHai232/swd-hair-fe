70819810e53048e9890dbf861446d9ee981592a1
Duchai232 <DucHai757@gmail.com>
2024-09-15 12:56:46 +0700

    localstorage

--- src/services/auth.service.js
+++ src/services/auth.service.js
@@ -3,6 +3,9 @@ import { api } from "./api.service";
 export const login = async (data) => {
   try {
     const response = await api.post(`/login`, data);
+    if (response.data.access_token) {
+      setTokenWithExpiry("access_token", response.data.access_token, 7);
+    }
     return response;
   } catch (error) {
     console.log("Login in service/auth error : ", error);
