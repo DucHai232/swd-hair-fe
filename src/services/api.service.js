140a9f30cabfc52a8ef7894a8e46bdc2d985acf6
dungtran2812 <dunghoclaptrinh2812@gmail.com>
2024-09-19 11:13:54 +0700

    create container for home, login,...

--- src/services/api.service.js
+++ src/services/api.service.js
@@ -1,7 +1,10 @@
 import axios from "axios";
+import axiosRetry from "axios-retry";
+import { setOnLineStatus } from "../feature/app";
+
 const url = import.meta.env.VITE_SERVER_URL;
 
-export const api = axios.create({
+export const axiosInstance = axios.create({
   baseURL: url,
   headers: {
     "Content-Type": "application/json",
@@ -9,7 +12,35 @@ export const api = axios.create({
   responseType: "json",
 });
 
-api.interceptors.request.use(
+axiosRetry(axiosInstance, {
+  retries: 5,
+  retryDelay: retryCount => {
+    return retryCount * 500;
+  },
+  shouldResetTimeout: true,
+  retryCondition: error => {
+    if (!navigator.onLine) {
+      dispatchEvent(setOnLineStatus(false))
+      return false;
+    }
+
+    const status = Number(error?.response?.status);
+    if (status === 503) {
+      return false;
+    }
+
+    return (
+      (status >= 100 && status <= 199) ||
+      (status >= 500 && status <= 599) ||
+      axiosRetry.isNetworkOrIdempotentRequestError(error) ||
+      status === 429 ||
+      status === 408 ||
+      status === 400
+    );
+  }
+});
+
+axiosInstance.interceptors.request.use(
   async (config) => {
     const accessToken = localStorage.getItem("access_token");
     if (accessToken) {
--- src/services/auth.service.js
+++ src/services/auth.service.js
@@ -1,8 +1,8 @@
-import { api } from "./api.service";
+import { axiosInstance } from "./api.service";
 
 export const login = async (data) => {
   try {
-    const response = await api.post(`/login`, data);
+    const response = await axiosInstance.post(`/login`, data);
     return response;
   } catch (error) {
     console.log("Login in service/auth error : ", error);
